1604dbd5ff2fe7cf678b57a7e0924d7c6e1f58e6
hirasheikh92 <hirasheikh92>
2024-04-18 07:19:13 +0000

    Pushed from Locofy Builder

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,19 +1,12 @@
 import { FunctionComponent, useCallback } from "react";
 import { Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import HeroContainer from "../components/HeroContainer";
 import SearchSectionHeader from "../components/SearchSectionHeader";
-import ListingItem from "../components/ListingItem";
+import CardList from "../components/CardList";
 import Footer from "../components/Footer";
 
 const Homepage: FunctionComponent = () => {
-  const navigate = useNavigate();
-
-  const onListingItemContainerClick = useCallback(() => {
-    navigate("/property-details");
-  }, [navigate]);
-
   const onShowMoreButtonClick = useCallback(() => {
     //TODO: please show more list items of onclick  this button
   }, []);
@@ -28,137 +21,7 @@ const Homepage: FunctionComponent = () => {
       <HeroContainer />
       <main className="self-stretch flex flex-col items-center justify-start py-[1.5rem] px-[5rem] gap-[1.375rem] mq750:pl-[2.5rem] mq750:pr-[2.5rem] mq750:box-border mq1275:pt-[1.25rem] mq1275:pb-[1.25rem] mq1275:box-border">
         <SearchSectionHeader />
-        <section className="w-full flex flex-row flex-wrap items-start justify-center gap-[1.25rem_1.168rem] max-w-[80rem] min-h-[105.75rem] mq1275:max-w-full">
-          <ListingItem
-            listingImage="/listing-image@2x.png"
-            listingTitle="Brightwoods Cabin"
-            listingSubtitle="Bridlepath, Ontario, Canada"
-            rating="4.9"
-            price="$658"
-            showBestTime
-            onListingItemContainerClick={onListingItemContainerClick}
-          />
-          <ListingItem
-            listingImage="/image@2x.png"
-            listingTitle="Urban Loft"
-            listingSubtitle="Georgina Bay, Ontario, Canada"
-            rating="4.5"
-            price="$410"
-            showBestTime={false}
-          />
-          <ListingItem
-            listingImage="/image-1@2x.png"
-            listingTitle="Forestville Cottages"
-            listingSubtitle="Simcoe, Ontario Canada"
-            rating="5.0"
-            price="$325"
-            showBestTime={false}
-          />
-          <ListingItem
-            listingImage="/image-2@2x.png"
-            listingTitle="Unionville Logde"
-            listingSubtitle="Markham, Ontario Canada"
-            rating="4.6"
-            price="$485"
-            showBestTime
-          />
-          <ListingItem
-            listingImage="/image-3@2x.png"
-            listingTitle="Missisuaga Aistream"
-            listingSubtitle="Missisauga, Ontario, Canada"
-            rating="4.8"
-            price="$502"
-            showBestTime
-          />
-          <ListingItem
-            listingImage="/image-4@2x.png"
-            listingTitle="Niagara Homes"
-            listingSubtitle="Niagara, Ontario, Canada"
-            rating="4.9"
-            price="$655"
-            showBestTime={false}
-          />
-          <ListingItem
-            listingImage="/image-5@2x.png"
-            listingTitle="Sunny Estate"
-            listingSubtitle="Barcort, Ontario Canada"
-            rating="5.0"
-            price="$320"
-            showBestTime
-          />
-          <ListingItem
-            listingImage="/image-6@2x.png"
-            listingTitle="Lawrence Hills"
-            listingSubtitle="Lawrence, Ontario Canada"
-            rating="5.0"
-            price="$350"
-            showBestTime={false}
-          />
-          <ListingItem
-            listingImage="/image-7@2x.png"
-            listingTitle="Simcoe Lake Lodge"
-            listingSubtitle="Simcoe, Ontario, Canada"
-            rating="5.0"
-            price="$395"
-            showBestTime={false}
-          />
-          <ListingItem
-            listingImage="/image-8@2x.png"
-            listingTitle="Wasaga Beach Home"
-            listingSubtitle="Georgina Bay, Ontario, Canada"
-            rating="5.0"
-            price="$385"
-            showBestTime
-          />
-          <ListingItem
-            listingImage="/image-9@2x.png"
-            listingTitle="Banff Hills"
-            listingSubtitle="Banff, Alberta, Canada"
-            rating="5.0"
-            price="$385"
-            showBestTime
-          />
-          <ListingItem
-            listingImage="/image-10@2x.png"
-            listingTitle="Creemore Canada"
-            listingSubtitle="Creemore, Alberta, Canada"
-            rating="5.0"
-            price="$385"
-            showBestTime={false}
-          />
-          <ListingItem
-            listingImage="/image-11@2x.png"
-            listingTitle="Kawartha Lakes"
-            listingSubtitle="Kawartha, Alberta,  Canada"
-            rating="5.0"
-            price="$385"
-            showBestTime={false}
-          />
-          <ListingItem
-            listingImage="/image-12@2x.png"
-            listingTitle="Revelstoke Cabin"
-            listingSubtitle="Revelstoke, Alberta, Canada"
-            rating="5.0"
-            price="$385"
-            showBestTime
-          />
-          <ListingItem
-            listingImage="/image-13@2x.png"
-            listingTitle="Brightwoods Estate"
-            listingSubtitle="Brightwoods Estate"
-            rating="5.0"
-            price="$385"
-            showBestTime={false}
-          />
-          <ListingItem
-            listingImage="/image-14@2x.png"
-            listingTitle="Brightwoods Estate"
-            listingSubtitle="Brightwoods Estate"
-            rating="5.0"
-            price="$385"
-            showBestTime={false}
-          />
-        </section>
+        <CardList />
         <Button
           className="w-[9.188rem] h-[3rem] cursor-pointer"
           disableElevation={true}
